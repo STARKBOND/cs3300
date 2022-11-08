e5a065208a7d35f8370e79ff7673dd672de1e83f
root <jbraseth@uccs.edu>
2022-11-08 17:00:34 +0000

    Added notification email method and tests.

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, :type => :mailer do
-  describe "should succeed (create email fields)" do
+  describe "should succeed (create welcome email fields)" do
     let(:mail) { UserMailer.welcome_email }
 
     it "renders the headers" do
@@ -12,6 +12,20 @@ RSpec.describe UserMailer, :type => :mailer do
 
     it "renders the body" do
       expect(mail.body.encoded).to match("Welcome to example.com")
+    end
+  end
+
+  describe "should succeed (create notify email fields)" do
+    let(:mail) { UserMailer.notify_email }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Check out these Project Portfolio examples!")
+      expect(mail.to).to eq(["user@example.com"])
+      expect(mail.from).to eq(["notifications@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Project portfolio examples")
     end
   end
 end
