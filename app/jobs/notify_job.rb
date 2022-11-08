8076f026834e29ce1512bdbd29b7b9e6ffa47c6f
root <jbraseth@uccs.edu>
2022-11-08 17:59:20 +0000

    Added tests for queueing jobs.

--- app/jobs/notify_job.rb
+++ app/jobs/notify_job.rb
@@ -1,7 +1,8 @@
 class NotifyJob < ApplicationJob
   queue_as :default
+  # NotifyJob.set(wait: 30.minutes).perform_later()
 
   def perform(*args)
-    # Do something later
+    UserMailer.notify_email().deliver_later
   end
 end
--- spec/jobs/notify_job_job_spec.rb
+++ spec/jobs/notify_job_job_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe NotifyJobJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe NotifyJob, :type => :job do
+  describe "should succeed (job is enqueued)" do
+    it "enqueues NotifyJob" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        NotifyJob.perform_later
+      }.to have_enqueued_job
+    end
+  end
+  
+  describe "should succeed (job is enqueued 30 minutes from now)" do
+    it "enqueues NotifyJob to perform in 30" do
+      later = 30.minutes.from_now
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        NotifyJob.set(wait_until: later).perform_later
+      }.to have_enqueued_job.at(later)
+    end
+  end
 end
