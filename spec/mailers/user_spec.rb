eeec2e9f8bfa3ac46441009cc879f965888ce00c
root <jbraseth@uccs.edu>
2022-11-08 16:36:48 +0000

    Finished test for application_mailer

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
-RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe UserMailer, :type => :mailer do
+  describe "should succeed (create email fields)" do
+    let(:mail) { UserMailer.welcome_email }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Welcome to your projects portfolio")
+      expect(mail.to).to eq(["user@example.com"])
+      expect(mail.from).to eq(["notifications@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Welcome to example.com")
+    end
+  end
 end
