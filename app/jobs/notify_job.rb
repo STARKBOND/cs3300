77cc194f309df845d0f3f07220bcfa69a4602646
root <jbraseth@uccs.edu>
2022-11-08 18:02:32 +0000

    Perform job method not needed

--- app/jobs/notify_job.rb
+++ app/jobs/notify_job.rb
@@ -1,8 +1,4 @@
 class NotifyJob < ApplicationJob
   queue_as :default
-  # NotifyJob.set(wait: 30.minutes).perform_later()
 
-  def perform(*args)
-    UserMailer.notify_email().deliver_later
-  end
 end
