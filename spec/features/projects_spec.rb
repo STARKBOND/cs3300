0a710181119c461d499ad6d7b339168f1c120c7f
root <jbraseth@uccs.edu>
2022-11-09 16:10:59 +0000

    Added mock user to project method tests to Green

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,65 +1,65 @@
 require 'rails_helper'
 
 RSpec.feature "Projects", type: :feature do
-  before(:each) do
-    @user = User.new(email: "user@email.com", password: "password")
-  end
-  context "Create new project" do
-    before(:each) do
-      visit new_project_path
-      within("form") do
-        fill_in "Title", with: "Test title"
+  before(:each) do # Requires :authenticate_user! before doing each action
+    @mock_user = User.new(email: "user@email.com", password: "password")
+    context "Create new project" do
+      before(:each) do
+        visit new_project_path
+        within("form") do
+          fill_in "Title", with: "Test title"
+        end
+      end
+
+      scenario "should be successful (title and description present)" do
+        fill_in "Description", with: "Test description"
+        click_button "Create Project"
+        expect(page).to have_content("Project was successfully created")
+      end
+
+      scenario "should fail (blank description)" do
+        click_button "Create Project"
+        expect(page).to have_content("Description can't be blank")
+      end
+
+      scenario "should fail (blank title)" do
+        fill_in "Title", with: ""
+        click_button "Create Project"
+        expect(page).to have_content("Title can't be blank")
       end
     end
 
-    scenario "should be successful (title and description present)" do
-      fill_in "Description", with: "Test description"
-      click_button "Create Project"
-      expect(page).to have_content("Project was successfully created")
-    end
-
-    scenario "should fail (blank description)" do
-      click_button "Create Project"
-      expect(page).to have_content("Description can't be blank")
-    end
-
-    scenario "should fail (blank title)" do
-      fill_in "Title", with: ""
-      click_button "Create Project"
-      expect(page).to have_content("Title can't be blank")
-    end
-  end
-
-  context "Update project" do
-    let(:project) { Project.create(title: "Test title", description: "Test content") }
-    before(:each) do
-      visit edit_project_path(project)
-    end
-
-    scenario "should be successful (update with description present)" do
-      within("form") do
-        fill_in "Description", with: "New description content"
+    context "Update project" do
+      let(:project) { Project.create(title: "Test title", description: "Test content") }
+      before(:each) do
+        visit edit_project_path(project)
       end
-      click_button "Update Project"
-      expect(page).to have_content("Project was successfully updated")
-    end
 
-    scenario "should fail (update with blank description)" do
-      within("form") do
-        fill_in "Description", with: ""
+      scenario "should be successful (update with description present)" do
+        within("form") do
+          fill_in "Description", with: "New description content"
+        end
+        click_button "Update Project"
+        expect(page).to have_content("Project was successfully updated")
       end
-      click_button "Update Project"
-      expect(page).to have_content("Description can't be blank")
-    end
-  end
 
-  context "Remove existing project" do
-    let!(:project) { Project.create(title: "Test title", description: "Test content") }
-    scenario "should be successful (0 projects after project was successfully destroyed)" do
-      visit project_path(project.id)
-      click_button "Destroy this project"
-      expect(page).to have_content("Project was successfully destroyed")
-      expect(Project.count).to eq(0)
+      scenario "should fail (update with blank description)" do
+        within("form") do
+          fill_in "Description", with: ""
+        end
+        click_button "Update Project"
+        expect(page).to have_content("Description can't be blank")
+      end
+    end
+
+    context "Remove existing project" do
+      let!(:project) { Project.create(title: "Test title", description: "Test content") }
+      scenario "should be successful (0 projects after project was successfully destroyed)" do
+        visit project_path(project.id)
+        click_button "Destroy this project"
+        expect(page).to have_content("Project was successfully destroyed")
+        expect(Project.count).to eq(0)
+      end
     end
   end
 end
