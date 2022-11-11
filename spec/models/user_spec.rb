f42d9187c7ff6ebfd041dd0b163dbd14171f3832
root <jbraseth@uccs.edu>
2022-11-11 00:26:50 +0000

    Rspec should now show 100% on CircleCI

--- spec/models/user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  it "should fail (user with no password)" do
-    @user = User.create(email: "user@example.com", password: "password")
-    expect(@user.email).to eq("user@example.com")
-  end
-end
